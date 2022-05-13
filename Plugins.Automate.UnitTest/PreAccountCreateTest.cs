8ffb58d1dbd0fd68f6fd57d080083d9b0bafd84c
Rajeev Pentyala <rajeevpe@microsoft.com>
2022-05-13 17:49:11 +0530

    Add project files.

---
+++ Plugins.Automate.UnitTest/PreAccountCreateTest.cs
@@ -0,0 +1,49 @@
+﻿namespace PPTemplateALMPlugins.UnitTest
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using Microsoft.Xrm.Sdk;
+    using Microsoft.Xrm.Sdk.Fakes;
+    using System.Collections.Generic;
+    using PPTemplate.ALM.PPTemplateALMPlugins;
+
+    [TestClass]
+    public class PreAccountCreateTest : UnitTestBase
+    {
+        [TestMethod]
+        public void Test_SetNameField()
+        {
+            Entity entAccount = new Entity("account");
+            entAccount["name"] = "No Hello World";
+
+            PluginExecutionContext.InputParametersGet = () => new ParameterCollection
+            {
+                new KeyValuePair<string, object>("Target", entAccount)
+            };
+            PluginExecutionContext.DepthGet = () => 1;
+            PluginExecutionContext.MessageNameGet = () => "create";
+            PluginExecutionContext.InitiatingUserIdGet = () => Guid.NewGuid();
+
+            PreOperationaccountCreate plugin = new PreOperationaccountCreate(string.Empty, string.Empty);
+            plugin.Execute(this.ServiceProvider);
+        }
+
+        [TestMethod]
+        public void Test_SetNameField_Error()
+        {
+            Entity entAccount = new Entity("account");
+            entAccount["name"] = "No Hello World";
+
+            PluginExecutionContext.InputParametersGet = () => new ParameterCollection
+            {
+                new KeyValuePair<string, object>("Target", entAccount)
+            };
+            PluginExecutionContext.DepthGet = () => 1;
+            PluginExecutionContext.MessageNameGet = () => "create";
+            PluginExecutionContext.InitiatingUserIdGet = () => Guid.NewGuid();
+
+            PreOperationaccountCreate plugin = new PreOperationaccountCreate(string.Empty, string.Empty);
+            plugin.Execute(this.ServiceProvider);
+        }
+    }
+}
---
+++ Plugins.Automate.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Plugins.Automate.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Plugins.Automate.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6e236d8a-79dd-4f26-afbb-c3b450117f09")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
